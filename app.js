b0f23372f82829d125b1d3e97d8d6c3439991409
Dan McKinney <3dhdaniel@gmail.com>
2019-12-13 11:00:16 +0100

    more freak

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-.connect('mongodb+srv://3dhdaniel:Chuhoop3!@cluster0-afta0.mongodb.net/mern?retryWrites=true&w=majority')
+.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-afta0.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
 
   .then(() => {
     app.listen(5000);
